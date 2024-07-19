cc0d5dd7db5a874679e2b0e7db67d70a3dc8a593
Jack Wright <jack.wright2@sky.uk>
2024-07-19 15:18:06 +0100

    DCLHOME-9999: written tests for fizz, buzz and fizzbuzz

--- src/fizz-buzz.test.js
+++ src/fizz-buzz.test.js
@@ -1,10 +1,29 @@
 const prod_code = (number) => {
-  if () {}
-  return "Fizz"; 
+  if (number % 3 === 0) {
+    return 'Fizz';
+  }
+
+  if (number % 5 === 0) {
+    return 'Buzz';
+  }
+
+  if (number % 5 === 0) {
+    return 'Buzz';
+  }
+
+  return 'no matches';
 };
 
 describe('Fizz Buzz', () => {
   it('Returns "Fizz" if the number is divisible by 3', () => {
     expect(prod_code(3)).toBe('Fizz');
+  });
+
+  it('Returns "Buzz" if the number is divisible by 5', () => {
+    expect(prod_code(5)).toBe('Buzz');
+  });
+
+  it('Returns "FizzBuzz" if the number is divisible by 3 and 5', () => {
+    expect(prod_code(15)).toBe('FizzBuzz');
   });
 });
